7bfb474cce0424bd2d1733d91d88c71c5363eb0f
will <williamknott00@gmail.com>
2021-01-31 23:40:45 +0000

    added nav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Nav class="sticky"></Nav>
     <!-- ======= Hero Section ======= -->
     <section id="hero" class="d-flex align-items-center" style="height: 1100px">
       <div
@@ -8,8 +9,7 @@
         data-aos-delay="200"
       >
         <h1>SKYSILL SOLUTIONS</h1>
-        <h2>est. 2020</h2>
-        <a href="#about" class="btn-get-started scrollto">Get Started</a>
+        <!-- <a href="#about" class="btn-get-started scrollto">Get Started</a> -->
       </div>
     </section>
     <!-- End Hero -->
@@ -507,248 +507,92 @@
           </div>
         </div>
       </section>
-       End Testimonials Section -->
-
-      <!-- ======= Team Section ======= -->
-      <!-- <section id="team" class="team">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-4">
-              <div class="section-title" data-aos="fade-right">
-                <h2>Team</h2>
-                <p>
-                  Necessitatibus eius consequatur ex aliquid fuga eum quidem.
-                </p>
-              </div>
-            </div>
-            <div class="col-lg-8">
-              <div class="row">
-                <div class="col-lg-6">
-                  <div class="member" data-aos="zoom-in" data-aos-delay="100">
-                    <div class="pic">
-                      <img
-                        src="assets/img/team/team-1.jpg"
-                        class="img-fluid"
-                        alt=""
-                      />
-                    </div>
-                    <div class="member-info">
-                      <h4>Walter White</h4>
-                      <span>Chief Executive Officer</span>
-                      <p>
-                        Explicabo voluptatem mollitia et repellat qui dolorum
-                        quasi
-                      </p>
-                      <div class="social">
-                        <a href=""><i class="ri-twitter-fill"></i></a>
-                        <a href=""><i class="ri-facebook-fill"></i></a>
-                        <a href=""><i class="ri-instagram-fill"></i></a>
-                        <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-lg-6 mt-4 mt-lg-0">
-                  <div class="member" data-aos="zoom-in" data-aos-delay="200">
-                    <div class="pic">
-                      <img
-                        src="assets/img/team/team-2.jpg"
-                        class="img-fluid"
-                        alt=""
-                      />
-                    </div>
-                    <div class="member-info">
-                      <h4>Sarah Jhonson</h4>
-                      <span>Product Manager</span>
-                      <p>
-                        Aut maiores voluptates amet et quis praesentium qui
-                        senda para
-                      </p>
-                      <div class="social">
-                        <a href=""><i class="ri-twitter-fill"></i></a>
-                        <a href=""><i class="ri-facebook-fill"></i></a>
-                        <a href=""><i class="ri-instagram-fill"></i></a>
-                        <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-lg-6 mt-4">
-                  <div class="member" data-aos="zoom-in" data-aos-delay="300">
-                    <div class="pic">
-                      <img
-                        src="assets/img/team/team-3.jpg"
-                        class="img-fluid"
-                        alt=""
-                      />
-                    </div>
-                    <div class="member-info">
-                      <h4>William Anderson</h4>
-                      <span>CTO</span>
-                      <p>
-                        Quisquam facilis cum velit laborum corrupti fuga rerum
-                        quia
-                      </p>
-                      <div class="social">
-                        <a href=""><i class="ri-twitter-fill"></i></a>
-                        <a href=""><i class="ri-facebook-fill"></i></a>
-                        <a href=""><i class="ri-instagram-fill"></i></a>
-                        <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-lg-6 mt-4">
-                  <div class="member" data-aos="zoom-in" data-aos-delay="400">
-                    <div class="pic">
-                      <img
-                        src="assets/img/team/team-4.jpg"
-                        class="img-fluid"
-                        alt=""
-                      />
-                    </div>
-                    <div class="member-info">
-                      <h4>Amanda Jepson</h4>
-                      <span>Accountant</span>
-                      <p>
-                        Dolorum tempora officiis odit laborum officiis et et
-                        accusamus
-                      </p>
-                      <div class="social">
-                        <a href=""><i class="ri-twitter-fill"></i></a>
-                        <a href=""><i class="ri-facebook-fill"></i></a>
-                        <a href=""><i class="ri-instagram-fill"></i></a>
-                        <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section> -->
-      <!-- End Team Section -->
+      End Testimonials Section -->
 
       <!-- ======= Contact Section ======= -->
-      <!-- <section id="contact" class="contact">
+      <section id="contact" class="contact">
         <div class="container">
           <div class="row">
-            <div class="col-lg-4" data-aos="fade-right">
+            <div class="col-lg-12" data-aos="fade-right">
               <div class="section-title">
                 <h2>Contact</h2>
                 <p>
-                  Magnam dolores commodi suscipit. Necessitatibus eius
-                  consequatur ex aliquid fuga eum quidem. Sit sint consectetur
-                  velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit
-                  suscipit alias ea. Quia fugiat sit in iste officiis commodi
-                  quidem hic quas.
+                  Ask us any questions you might have and our team will get back
+                  to you as soon as possible.
                 </p>
               </div>
-            </div>
-
-            <div class="col-lg-8" data-aos="fade-up" data-aos-delay="100">
-              <iframe
-                style="border: 0; width: 100%; height: 270px"
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
-                frameborder="0"
-                allowfullscreen
-              ></iframe>
-              <div class="info mt-4">
-                <i class="icofont-google-map"></i>
-                <h4>Location:</h4>
-                <p>A108 Adam Street, New York, NY 535022</p>
-              </div>
-              <div class="row">
-                <div class="col-lg-6 mt-4">
-                  <div class="info">
-                    <i class="icofont-envelope"></i>
-                    <h4>Email:</h4>
-                    <p>info@example.com</p>
+              <div class="col-lg-12" data-aos="fade-up" data-aos-delay="100">
+                <form
+                  action="forms/contact.php"
+                  method="post"
+                  role="form"
+                  class="php-email-form mt-4"
+                >
+                  <div class="form-row">
+                    <div class="col-md-6 form-group">
+                      <input
+                        type="text"
+                        name="name"
+                        class="form-control"
+                        id="name"
+                        placeholder="Your Name"
+                        data-rule="minlen:4"
+                        data-msg="Please enter at least 4 chars"
+                      />
+                      <div class="validate"></div>
+                    </div>
+                    <div class="col-md-6 form-group">
+                      <input
+                        type="email"
+                        class="form-control"
+                        name="email"
+                        id="email"
+                        placeholder="Your Email"
+                        data-rule="email"
+                        data-msg="Please enter a valid email"
+                      />
+                      <div class="validate"></div>
+                    </div>
                   </div>
-                </div>
-                <div class="col-lg-6">
-                  <div class="info w-100 mt-4">
-                    <i class="icofont-phone"></i>
-                    <h4>Call:</h4>
-                    <p>+1 5589 55488 55s</p>
-                  </div>
-                </div>
-              </div>
-
-              <form
-                action="forms/contact.php"
-                method="post"
-                role="form"
-                class="php-email-form mt-4"
-              >
-                <div class="form-row">
-                  <div class="col-md-6 form-group">
+                  <div class="form-group">
                     <input
                       type="text"
-                      name="name"
                       class="form-control"
-                      id="name"
-                      placeholder="Your Name"
+                      name="subject"
+                      id="subject"
+                      placeholder="Subject"
                       data-rule="minlen:4"
-                      data-msg="Please enter at least 4 chars"
+                      data-msg="Please enter at least 8 chars of subject"
                     />
                     <div class="validate"></div>
                   </div>
-                  <div class="col-md-6 form-group">
-                    <input
-                      type="email"
+                  <div class="form-group">
+                    <textarea
                       class="form-control"
-                      name="email"
-                      id="email"
-                      placeholder="Your Email"
-                      data-rule="email"
-                      data-msg="Please enter a valid email"
-                    />
+                      name="message"
+                      rows="5"
+                      data-rule="required"
+                      data-msg="Please write something for us"
+                      placeholder="Message"
+                    ></textarea>
                     <div class="validate"></div>
                   </div>
-                </div>
-                <div class="form-group">
-                  <input
-                    type="text"
-                    class="form-control"
-                    name="subject"
-                    id="subject"
-                    placeholder="Subject"
-                    data-rule="minlen:4"
-                    data-msg="Please enter at least 8 chars of subject"
-                  />
-                  <div class="validate"></div>
-                </div>
-                <div class="form-group">
-                  <textarea
-                    class="form-control"
-                    name="message"
-                    rows="5"
-                    data-rule="required"
-                    data-msg="Please write something for us"
-                    placeholder="Message"
-                  ></textarea>
-                  <div class="validate"></div>
-                </div>
-                <div class="mb-3">
-                  <div class="loading">Loading</div>
-                  <div class="error-message"></div>
-                  <div class="sent-message">
-                    Your message has been sent. Thank you!
+                  <div class="mb-3">
+                    <div class="loading">Loading</div>
+                    <div class="error-message"></div>
+                    <div class="sent-message">
+                      Your message has been sent. Thank you!
+                    </div>
                   </div>
-                </div>
-                <div class="text-center">
-                  <button type="submit">Send Message</button>
-                </div>
-              </form>
+                  <div class="text-center">
+                    <button type="submit">Send Message</button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </section> -->
+      </section>
       <!-- End Contact Section -->
     </main>
     <!-- End #main -->
@@ -848,7 +692,19 @@
 </template>
 
 <script>
+import Nav from "@/components/nav.vue";
+
 export default {
   name: "Home",
+  components: {
+    Nav,
+  },
 };
 </script>
+
+<style scoped>
+.sticky {
+  position: fixed;
+  z-index: 1000;
+}
+</style>
